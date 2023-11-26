4633e43cbd1ab86123edc913136a1a86f66ec64e
Francisco Alcaraz <whothefluff@gmail.com>
2023-11-26 12:11:48 +0100

    add creation timestamp tooltip adjust last message sizing in all screen sizes

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -44,7 +44,9 @@ function initializeTooltips() {
 
 function isThisWeek(date) {
     const now = new Date();
-    const startOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay());
+    const startOfWeek = new Date(now);
+    startOfWeek.setDate(now.getDate() - (now.getDay() === 0 ? 6 : now.getDay() - 1));
+    startOfWeek.setHours(0, 0, 0, 0);
     const endOfWeek = new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 7);
     return date >= startOfWeek && date < endOfWeek;
 }
@@ -76,7 +78,7 @@ function displayTime(timestamp) {
             minute: '2-digit',
             hour12: false
         };
-        return timestampDate.toLocaleString(undefined, options);
+        return 'on ' + timestampDate.toLocaleString(undefined, options);
     }
 }
 
